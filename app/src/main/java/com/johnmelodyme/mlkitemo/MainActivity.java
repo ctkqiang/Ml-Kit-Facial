4646b986e6141caf6057dd2ee5618e852997b402
John Melody Me <johnmelodyme@icloud.com>
2020-01-31 01:16:09 +0800

    Uploaded

--- app/src/main/java/com/johnmelodyme/mlkitemo/MainActivity.java
+++ app/src/main/java/com/johnmelodyme/mlkitemo/MainActivity.java
@@ -3,7 +3,6 @@ package com.johnmelodyme.mlkitemo;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.google.firebase.FirebaseApp;
 
 /**
